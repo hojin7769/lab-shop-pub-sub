ce3d2c1704f478d95e4e97b9188ed6a101cddd6d
HoJin Park <85658845+hojin7769@users.noreply.github.com>
2022-11-04 08:56:13 +0900

    commit

---
+++ delivery/src/main/java/labshoppubsub/domain/Delivery.java
@@ -0,0 +1,82 @@
+package labshoppubsub.domain;
+
+import labshoppubsub.domain.DeliveryStarted;
+import labshoppubsub.DeliveryApplication;
+import javax.persistence.*;
+import java.util.List;
+import lombok.Data;
+import java.util.Date;
+
+@Entity
+@Table(name="Delivery_table")
+@Data
+
+public class Delivery  {
+
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    
+    
+    
+    
+    
+    private Long id;
+    
+    
+    
+    
+    
+    private String customerId;
+    
+    
+    
+    
+    
+    private String orderId;
+
+    @PostPersist
+    public void onPostPersist(){
+
+
+        DeliveryStarted deliveryStarted = new DeliveryStarted(this);
+        deliveryStarted.publishAfterCommit();
+
+    }
+
+    public static DeliveryRepository repository(){
+        DeliveryRepository deliveryRepository = DeliveryApplication.applicationContext.getBean(DeliveryRepository.class);
+        return deliveryRepository;
+    }
+
+
+
+
+    public static void addToDeliveryList(OrderPlaced orderPlaced){
+
+        /** Example 1:  new item 
+        Delivery delivery = new Delivery();
+        repository().save(delivery);
+
+        DeliveryStarted deliveryStarted = new DeliveryStarted(delivery);
+        deliveryStarted.publishAfterCommit();
+        */
+
+        /** Example 2:  finding and process
+        
+        repository().findById(orderPlaced.get???()).ifPresent(delivery->{
+            
+            delivery // do something
+            repository().save(delivery);
+
+            DeliveryStarted deliveryStarted = new DeliveryStarted(delivery);
+            deliveryStarted.publishAfterCommit();
+
+         });
+        */
+
+        
+    }
+
+
+}
